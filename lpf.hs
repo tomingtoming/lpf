0f25fb61a5f483d1cb1b5f82ddced289b61d2803
toming <tomingtoming@gmail.com>
2013-09-27 14:03:18 +0900

    Reflesh by Conduit

--- lpf.hs
+++ lpf.hs
@@ -1,27 +1,16 @@
-import System.IO
-import System.Environment
-import Control.Concurrent
-import Network
-import qualified Data.ByteString as Bs
+import Network (withSocketsDo)
+import Data.Conduit (($$))
+import Data.Conduit.Network
+import qualified Data.ByteString.Char8 as C8
+import Control.Concurrent.Async (race_)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = withSocketsDo $ do
-  (lp:rh:rp:_) <- getArgs
-  socket <- listenOn $ PortNumber $ fromInteger $ read lp
-  mainLoop socket rh $ PortNumber $ fromInteger $ read rp
-
-mainLoop :: Socket -> String -> PortID -> IO ()
-mainLoop socket hostname port = do
-  (lh, ch, cp) <- accept socket
-  putStrLn $ "Accept: " ++ ch ++ ":" ++ (show cp)
-  rh <- connectTo hostname port
-  _ <- forkIO $ hCopy lh rh
-  _ <- forkIO $ hCopy rh lh
-  mainLoop socket hostname port
-
-hCopy :: Handle -> Handle -> IO ()
-hCopy from to = do
-  eof <- hIsEOF from
-  if(eof)
-  then return ()
-  else Bs.hGetSome from (1024*256) >>= Bs.hPut to >> hFlush to >> hCopy from to
+  (localPort:remoteHost:remotePort:_) <- getArgs
+  let
+    srvConf = serverSettings (read localPort) HostAny
+    cliConf = clientSettings (read remotePort) (C8.pack remoteHost)
+    cliApp  = \cli -> runTCPClient cliConf (srvApp cli)
+    srvApp cli = \srv -> (appSource cli $$ appSink srv) `race_` (appSource srv $$ appSink cli)
+  runTCPServer srvConf cliApp
